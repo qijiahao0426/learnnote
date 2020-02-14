0d308164dd6421144129db9707f62d1e3be1a571
qijiahao <qijiahao0426@iCloud.com>
2020-02-14 21:03:33 +0800

    正式版

--- note/PAT B1032 挖掘机技术哪家强.cpp
+++ note/PAT B1032 挖掘机技术哪家强.cpp
@@ -3,8 +3,6 @@ const int maxn=100010;
 int school[maxn]={0};
 int main(){
     int n,schID,score;
-    printf("！！！！！");
-    printf("--------");
     scanf("%d",&n);
     for(int i=0;i<n;i++){
         scanf("%d%d",&schID,&score);
