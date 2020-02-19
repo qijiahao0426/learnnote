c202e84947518019fbf313bc512921b302f4df34
qijiahao <qijiahao0426@iCloud.com>
2020-02-19 17:04:40 +0800

    头文件

---
+++ C++基础/头文件.cpp
@@ -0,0 +1,17 @@
+/**
+ *  # 表示预编译
+ * 头文件的内容：
+ *          全局数据声明，如 extern int n; extern int a[];
+ *          函数声明，如 void fn()
+ *          类型声明，如 class A
+ *          全局常数定义，如 const float pi=3.14
+ *          内联函数定义，如 inline void fn()
+ *          模版申明和定义，如 template<class T> class A{...};
+ *          名空间定义，如 namespace N{...}
+ *          类型定义，如 enum COLOR{...}; class A{...};
+ *          预编译指令，如 #include<iostream>
+ *          注释，如 // 2020年加油！！
+ * 头文件中绝对不能加的内容：
+ *          全局数据定义，如 int a；
+ *          函数定义，如 void fn(){...}
+ **/
