c82555e024101d129db94f6b83db98874b76fa4b
qijiahao <qijiahao0426@iCloud.com>
2020-02-22 22:58:07 +0800

    坐标上升优化以及smo优化实现

--- 机器学习/SVM/svmMLiA.py
+++ 机器学习/SVM/svmMLiA.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from numpy import *
 from matplotlib import *
 
@@ -30,64 +31,43 @@ def clipAlpha(aj,H,L):
 #   函数介绍：
 #   mat():构造矩阵    transpose()和.T:矩阵转置    shape():查看矩阵纬度
 #   multiply():数组和矩阵对应位置相乘，输出与相乘数组/矩阵的大小一致
-def smoSimple(dataMatIn,classLabels,C,toler,maxIter):# 数据集，类别标签，常数C，容错率和退出前最大的循环次数
-    dataMatrix=mat(dataMatIn);labelMat=mat(classLabels).transpose()# 转置是为了使标签和数据集矩阵一一对应
-    b=0;m,n=shape(dataMatrix)# 通过shape得到维度
-    alphas=mat(zeros((m,1)))
-    iter=0 # iter存储在没有任何alpha改变情况下遍历数据集的次数，当它达到输入值MaxIter时，函数结束运行并退出
-    while (iter<maxIter):
-        alphaPairsChanged=0 #预设为0
-        for i in range(m):# 对整个集合顺序遍历
-            fXi=float(multiply(alphas,labelMat).T*\
-                (dataMatrix*dataMatrix[i,:].T))+b #选择使用线性内核
-            Ei=fXi-float(labelMat[i])# 误差Ei，如果误差很大，那么可以对该数据实例所对应的alpha值进行优化
-            #在if语句中，不管是正间隔还是负间隔都会被测试。同时检查alpha值，以保证其不能等于0或C。由于后面alpha小于0或大于 
-            #C时将被调整为0或C，所以一旦在该if语句中它们等于这两个值的话，那么它们就已经在“边界” 上了，因而不再能够减小或增大，因此也就不值得再对它们进行优化了。
-            if((labelMat[i]*Ei<-toler) and (alphas[i]<C)) or\  
-               ((labelMat[i]*Ei>toler) and\
-                (alphas[i]>0)):
-                j=selectJrand(i,m) #随机选择第二个alpha值,即alpha[j]
-                # 可以采用第一个alpha值（alpha[i]）的误差计算方法，来计算这个alpha值的误差
-                fXj=float(multiply(alphas,labelMat).T*\
-                (dataMatrix*dataMatrix[j,:].T))+b 
-                Ej=fXj-float(labelMat[j])
-                alphaIold=alphas[i].copy();
-                alphaJold=alphas[j].copy();
-                # 保证alpha在0与C之间
-                if(labelMat[i]!=labelMat[j]):
-                    L=max(0,alphas[j]-alphas[i])
-                    H=min(C,C+alphas[j]-alphas[i])
+def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+    dataMatrix = mat(dataMatIn); labelMat = mat(classLabels).transpose()
+    b = 0; m,n = shape(dataMatrix)
+    alphas = mat(zeros((m,1)))
+    iter = 0
+    while (iter < maxIter):
+        alphaPairsChanged = 0
+        for i in range(m):
+            fXi = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[i,:].T)) + b
+            Ei = fXi - float(labelMat[i])#if checks if an example violates KKT conditions
+            if ((labelMat[i]*Ei < -toler) and (alphas[i] < C)) or ((labelMat[i]*Ei > toler) and (alphas[i] > 0)):
+                j = selectJrand(i,m)
+                fXj = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[j,:].T)) + b
+                Ej = fXj - float(labelMat[j])
+                alphaIold = alphas[i].copy(); alphaJold = alphas[j].copy();
+                if (labelMat[i] != labelMat[j]):
+                    L = max(0, alphas[j] - alphas[i])
+                    H = min(C, C + alphas[j] - alphas[i])
                 else:
-                    L=max(0,alphas[j]+alphas[i]-C)
-                    H=min(C,alphas[j]+alphas[i])
-                if L==H: print "L==H";continue
-                eta=2.0*dataMatrix[i,:]*dataMatrix[j,:].T- \
-                    dataMatrix[i,:]*dataMatrix[i,:].T - \
-                    dataMatrix[j,:]*dataMatrix[j,:].T # Eta是alpha[j]的最优修改量，该过程对真实SMO算法进行了简化处理。如果eta为0，那么 计算新的alpha[j]就比较麻烦了，这里我们就不对此进行详细的介绍了
-                if eta>=0: print "eta>=0";continue
-                alphas[j]-=labelMat[j]*(Ei-Ej)/eta
-                alphas[j]=clipAlpha(alphas[j],H,L)
-                if(abs(alphas[j]-alphaJold)<0.00001):print \
-                        "j not moving enough";continue
-                alphas[i]+=labelMat[j]*labelMat[i]*\
-                        (alphaJold-alphas[j])
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
+                    L = max(0, alphas[j] + alphas[i] - C)
+                    H = min(C, alphas[j] + alphas[i])
+                if L==H: print "L==H"; continue
+                eta = 2.0 * dataMatrix[i,:]*dataMatrix[j,:].T - dataMatrix[i,:]*dataMatrix[i,:].T - dataMatrix[j,:]*dataMatrix[j,:].T
+                if eta >= 0: print "eta>=0"; continue
+                alphas[j] -= labelMat[j]*(Ei - Ej)/eta
+                alphas[j] = clipAlpha(alphas[j],H,L)
+                if (abs(alphas[j] - alphaJold) < 0.00001): print "j not moving enough"; continue
+                alphas[i] += labelMat[j]*labelMat[i]*(alphaJold - alphas[j])#update i by the same amount as j
+                                                                        #the update is in the oppostie direction
+                b1 = b - Ei- labelMat[i]*(alphas[i]-alphaIold)*dataMatrix[i,:]*dataMatrix[i,:].T - labelMat[j]*(alphas[j]-alphaJold)*dataMatrix[i,:]*dataMatrix[j,:].T
+                b2 = b - Ej- labelMat[i]*(alphas[i]-alphaIold)*dataMatrix[i,:]*dataMatrix[j,:].T - labelMat[j]*(alphas[j]-alphaJold)*dataMatrix[j,:]*dataMatrix[j,:].T
+                if (0 < alphas[i]) and (C > alphas[i]): b = b1
+                elif (0 < alphas[j]) and (C > alphas[j]): b = b2
+                else: b = (b1 + b2)/2.0
+                alphaPairsChanged += 1
+                print "iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged)
+        if (alphaPairsChanged == 0): iter += 1
+        else: iter = 0
+        print "iteration number: %d" % iter
+    return b,alphas                
