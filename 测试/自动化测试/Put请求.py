e386e0933c351b7c35023e80562d160d63082b77
qijiahao <qijiahao0426@iCloud.com>
2020-02-26 15:38:43 +0800

    put请求

---
+++ 测试/自动化测试/Put请求.py
@@ -0,0 +1,16 @@
+# 1.导包
+import requests
+# 2. 调用post
+url="http://XXXXX/id"
+headers={"Content-Type":"application/json"}
+data={
+    "data":[{
+                "dep_id":"id"
+                "dep_name":"Test学院"
+            }]
+    }
+r=requests.put(url,json=data,headers=headers)
+# 3.获取响应对象
+print(r.json)
+# 4.获取响应状态码
+print(r.status_code)
