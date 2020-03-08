9f62d064c8a5d9afdcceb4da38b5cb52ae1a225c
qijiahao <qijiahao0426@iCloud.com>
2020-03-08 20:13:26 +0800

    有效的括号

--- leetCode/20.有效的括号.c
+++ leetCode/20.有效的括号.c
@@ -29,7 +29,39 @@ int isValid(char * s){
     if(top>=0) return 0;
     return 1;
 }
+/**
+ * 
+ * if(!s || s[0] == "")
+        return 1;
+    char* stk = (char*)malloc(strlen(s));
+    int top = -1;
+    int i;
+    for(i = 0; s[i] != '\0'; i++){
+        switch(s[i]){
+            case '(':
+            case '[':
+            case '{':
+                stk[++top] = s[i];
+                break;
+            case ')':
+                if(top < 0 || stk[top--] != '(')
+                    return 0;
+                break;
+            case ']':
+                if(top < 0 || stk[top--] != '[')
+                    return 0;
+                break;
+            case '}':
+                if(top < 0 || stk[top--] != '{')
+                    return 0;
+                break;
+        }
+    }
+    if(top >= 0)
+        return 0;   
+    return 1;
 
+ * /
 
 // @lc code=end
 
