56baf9534bfdb040c9d8169c382b5f44689bd924
qijiahao <qijiahao0426@iCloud.com>
2020-03-08 20:53:52 +0800

    test

--- 算法笔记/test.c
+++ 算法笔记/test.c
@@ -1,41 +1,29 @@
 #include<stdio.h>
-
+#include <stdlib.h>
 #include<string.h>
-char * longestCommonPrefix(char ** strs, int strsSize){
-    if(strsSize==0){
-        return "";
-    }
-    char *str0=strs[0];
-    int l=0;
-    while(*str0){
-        str0++;
-        l++;
-    }
-    str0=strs[0];
-    for(int i=1;i<strsSize;i++){
-        int j=0;
-        while(*(str0+j)!='\0'&&*(strs[i]+j)!='\0'&&j<l){
-            if(*(str0+j)!=strs[i][j]){
-                break;
-            }else{
-                j++;
-            }
-        }
-        l=j;
-
-        if(l==0){
-            return "";
+int isValid(char * s){
+    if(s=="") return 1;
+    char *stack=(char*)malloc(strlen(s));
+    int top=-1;
+    for(int i=0;s[i]!='\0';i++){
+        if(s[i]=='('||s[i]=='['||s[i]=='{'){
+            stack[++top]=s[i];
+        }else if(top>=0){
+            if(s[i]==')'&&stack[top--]!='('){
+                return 0;
+            }else if(s[i]==']'&&stack[top--]!='['){
+                return 0;
+            }else if(s[i]=='}'&&stack[top--]!='{'){
+                return 0;
         }
     }
-    char string[10000];
-    strncpy(string, str0, l);
-    string[l]='\0';
-    return string;
+    }
+    if(top>=0) return 0;
+    return 1;
 }
 
-
 int main(){
-    char *a[10000]={"flower","flow","flight"};
-    char *b=longestCommonPrefix(a,3);
-    printf("%s",b);
+    char a[10]="{[()]}";
+    printf("%d",isValid(a));
+    return 0;
 }
