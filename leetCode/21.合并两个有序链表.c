d5920d26db2c2ef68be01634efc297fa052ce506
qijiahao <qijiahao0426@iCloud.com>
2020-03-11 14:20:21 +0800

    合并两个有序链表

---
+++ leetCode/21.合并两个有序链表.c
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=c
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+#include<stdlib.h>
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
+    if (!l1)
+		return l2;
+	if (!l2)
+		return l1;
+    struct ListNode *head=(struct ListNode*)malloc(sizeof(struct ListNode)),*p=head;
+    while(l1&&l2){
+        if(l2->val>l1->val){
+            p->next=l1;
+            l1=l1->next;
+        }else
+        {
+            p->next=l2;
+            l2=l2->next;
+        }
+        p=p->next;
+        
+    }
+    if(l1) p->next=l1;
+    if(l2) p->next=l2;
+    return head->next;
+}
+
+
+// @lc code=end
+
