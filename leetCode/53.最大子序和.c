f60a9742789024724c4814580d8b38efa7ffa921
qijiahao <qijiahao0426@iCloud.com>
2020-03-14 16:50:44 +0800

    最大子序和(动态规划)

--- leetCode/53.最大子序和.c
+++ leetCode/53.最大子序和.c
@@ -7,6 +7,8 @@
 // @lc code=start
 
 #include<limits.h>
+#include<stdlib.h>
+#include<string.h>
 int maxSubArray(int* nums, int numsSize){
     int max=INT_MIN;
     // for(int i=0;i<numsSize;i++){
@@ -18,17 +20,26 @@ int maxSubArray(int* nums, int numsSize){
     //         }
     //     }
     // }
-    int sum=0;
-    for(int i=0;i<numsSize;i++){
-        sum+=nums[i];
-        if(max<sum){
-            max=sum;
-        }
-        if(sum<0){
-            sum=0;
-        }
+
+    // int sum=0;
+    // for(int i=0;i<numsSize;i++){
+    //     sum+=nums[i];
+    //     if(max<sum){
+    //         max=sum;
+    //     }
+    //     if(sum<0){
+    //         sum=0;
+    //     }
+    // }
+    int *dp=(int*)malloc(sizeof(int)*numsSize);
+    memset(dp,0,numsSize);
+    dp[0] = nums[0];
+    int result=dp[0];
+    for(int i=1;i<numsSize;i++){
+        dp[i]=(dp[i-1]+nums[i]>nums[i]?dp[i-1]+nums[i]:nums[i]);
+        if(result<dp[i]) result=dp[i];
     }
-    return max;
+    return result;
 }
 
 
