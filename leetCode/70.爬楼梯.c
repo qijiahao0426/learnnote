13e455c351a427c2883beba9573890813b102346
qijiahao <qijiahao0426@iCloud.com>
2020-03-15 22:18:06 +0800

    爬楼梯（添加递归实现）

--- leetCode/70.爬楼梯.c
+++ leetCode/70.爬楼梯.c
@@ -18,6 +18,28 @@ int climbStairs(int n){
     }
     return dp[n];
 }
+/*
+int _climb(int n, int *arr)
+{
+    if (arr[n] != 0 ) return arr[n];
+    arr[n] = _climb(n-1, arr) + _climb(n-2, arr);
+    return arr[n];
+
+}
+
+int climbStairs(int n){
+
+    //????
+    if ( n <  0 ) return 0;
+    if ( n <= 2) return n;
+    int *arr = (int*)calloc(n+1, sizeof(int));
+    arr[1] = 1;
+    arr[2] = 2;
+    return _climb(n , arr);
+
+}
+*/
+
 
 
 // @lc code=end
