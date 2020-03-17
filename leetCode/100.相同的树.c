5cdd7988d69083d6b6c802621f35d288b4ed8e95
qijiahao <qijiahao0426@iCloud.com>
2020-03-18 02:09:31 +0800

    相同的树（优化）

--- leetCode/100.相同的树.c
+++ leetCode/100.相同的树.c
@@ -18,8 +18,7 @@
 int isSameTree(struct TreeNode* p, struct TreeNode* q){
     if(p==0&&q==0) return 1;
     if(p==0||q==0) return 0;
-    if(p->val!=q->val) return 0;
-    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    return p->val==q->val&&isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
 }
 
 
