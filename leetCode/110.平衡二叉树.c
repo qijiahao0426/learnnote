f1d190818cb8a279232f06499758f7e4e036e950
qijiahao <qijiahao0426@iCloud.com>
2020-03-20 14:24:37 +0800

    平衡二叉树

---
+++ leetCode/110.平衡二叉树.c
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=c
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+#include<math.h>
+
+//  struct TreeNode {
+//      int val;
+//      struct TreeNode *left;
+//      struct TreeNode *right;
+//  };
+int isBalanced(struct TreeNode* root){
+    if(root==0) return 1;
+    return (abs(depth(root->left)-depth(root->right))<=1)&&isBalanced(root->left)&&isBalanced(root->right);
+}
+
+int depth(struct TreeNode* root){
+    if(root==0) return 0;
+    int l=depth(root->left);
+    int r=depth(root->right);
+    return (l>r?l:r)+1;
+}
+
+
+// @lc code=end
+
