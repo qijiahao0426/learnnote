2982f68701e72bad05f4c1f021525079daeff893
qijiahao <qijiahao0426@iCloud.com>
2020-03-22 20:50:44 +0800

    买卖股票的最佳时机（动态规划）

--- leetCode/121.买卖股票的最佳时机.c
+++ leetCode/121.买卖股票的最佳时机.c
@@ -8,15 +8,25 @@
 
 
 int maxProfit(int* prices, int pricesSize){
+    if(pricesSize==0) return 0;
+    // int max=0;
+    // for(int i=0;i<pricesSize-1;i++){
+    //     for(int j=i+1;j<pricesSize;j++){
+    //         if(prices[i]<prices[j]&&prices[j]-prices[i]>max){
+    //             max=prices[j]-prices[i];
+    //         }
+    //     }
+    // }
+    // return max;
     int max=0;
-    for(int i=0;i<pricesSize-1;i++){
-        for(int j=i+1;j<pricesSize;j++){
-            if(prices[i]<prices[j]&&prices[j]-prices[i]>max){
-                max=prices[j]-prices[i];
-            }
-        }
+    int min=prices[0];
+    int* dp=(int*)calloc((pricesSize+1),sizeof(int));
+    for(int i=1;i<pricesSize;i++){
+        min=min<prices[i]?min:prices[i];
+        int tmp=prices[i]-min;
+        dp[i]=dp[i-1]>tmp?dp[i-1]:tmp;
     }
-    return max;
+    return dp[pricesSize-1];
 }
 
 
