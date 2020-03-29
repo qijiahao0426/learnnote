62603c6ad5029d7b35ece518a080dadaf81014b0
qijiahao0426 <qijiahao0426@icloud.com>
2020-03-29 09:01:29 +0800

    多数元素 hash

--- leetCode/169.多数元素.py
+++ leetCode/169.多数元素.py
@@ -7,19 +7,22 @@
 # @lc code=start
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        l=len(nums)
-        i=0
-        temp=0
-        n=0;
-        while(i<l):
-            if(n==0):
-                temp=nums[i]
-            if(nums[i]!=temp):
-                n-=1
-            else:
-                n+=1
-            i+=1
-        return temp
+        # l=len(nums)
+        # i=0
+        # temp=0
+        # n=0;
+        # while(i<l):
+        #     if(n==0):
+        #         temp=nums[i]
+        #     if(nums[i]!=temp):
+        #         n-=1
+        #     else:
+        #         n+=1
+        #     i+=1
+        # return temp
+        counts = collections.Counter(nums)
+        return max(counts.keys(), key=counts.get)
+
 
 # @lc code=end
 
