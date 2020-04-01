11db658e81aaa27489604e048b0d369539978a81
qijiahao0426 <qijiahao0426@icloud.com>
2020-04-01 21:07:18 +0800

    计数质数 优化版本

--- leetCode/204.计数质数.py
+++ leetCode/204.计数质数.py
@@ -7,17 +7,13 @@
 # @lc code=start
 class Solution:
     def countPrimes(self, n: int) -> int:
-        nums = 0
-        for i in range(2,n):
-            if i == 2:
-                nums += 1
-                continue
-            for j in range(2,int(i**0.5)+1):
-                if i%j == 0:
-                    break
-            else:
-                print(i)
-                nums += 1
-        return nums
+        if n < 3:
+            return 0
+        results = [1]*n
+        results[0],results[1] = 0, 1
+        for i in range(2,int(n**0.5)+1):
+            if results[i] == 1:
+                results[i*2:n:i] = [0]*len(results[i*2:n:i])
+        return sum(results)-1
 # @lc code=end
 
