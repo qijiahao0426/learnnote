665f40314f49eecbe8ebc99b9c939059b597749a
qijiahao0426 <qijiahao0426@icloud.com>
2020-04-02 19:44:02 +0800

    反转链表

---
+++ leetCode/206.反转链表.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=206 lang=python3
+#
+# [206] 反转链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pre=None
+        p=head
+        while p:
+            tmp=p.next
+            p.next=pre
+            pre=p
+            p=tmp
+        return pre
+
+# @lc code=end
+
