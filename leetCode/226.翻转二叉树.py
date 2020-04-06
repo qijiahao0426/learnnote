2cdd6cbeea043718c213531efee5930f41ec37b0
qijiahao0426 <qijiahao0426@icloud.com>
2020-04-06 23:44:19 +0800

    翻转二叉树

---
+++ leetCode/226.翻转二叉树.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=226 lang=python3
+#
+# [226] 翻转二叉树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root==None:
+            return None
+        left=self.invertTree(root.left)
+        right=self.invertTree(root.right)
+        root.left=right
+        root.right=left
+        return root
+# @lc code=end
+
